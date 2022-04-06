c1bd34df1e6fec5c45107a9b7f80c6a9a82d6406
Ayush Gautam <Ayush.Gautam@betsol.com>
2022-04-06 14:19:46 +0530

    init

--- app.py
+++ app.py
@@ -6,29 +6,23 @@ import logging
 from logger_config import custom_logger
 
 
-# db = SQLAlchemy()
-# migrate = Migrate()
+db = SQLAlchemy()
+migrate = Migrate()
 
-# logger = logging.getLogger('static')
-# logger = custom_logger(logger)
+logger = logging.getLogger('static')
+logger = custom_logger(logger)
 
 
-# def create_app(test_config=None):
-#     app = Flask(__name__)
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://maepgdupzvcqru:2de24244b6ca685cd41dc98ae29c30c448ded6a6bcb0ff02afbd66dd5a35730f@ec2-52-73-155-171.compute-1.amazonaws.com:5432/dbmevkl5rhd3tg'
-#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://maepgdupzvcqru:2de24244b6ca685cd41dc98ae29c30c448ded6a6bcb0ff02afbd66dd5a35730f@ec2-52-73-155-171.compute-1.amazonaws.com:5432/dbmevkl5rhd3tg'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#     from todoApp.model.todo_list_model import Todo
-#     db.init_app(app)
-#     migrate.init_app(app, db)
+    from todoApp.model.todo_list_model import Todo
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-#     from todoApp import todo_list
-#     app.register_blueprint(todo_list, url_prefix="/api/v1")
+    from todoApp import todo_list
+    app.register_blueprint(todo_list, url_prefix="/api/v1")
 
-#     return app
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "hi"
+    return app
