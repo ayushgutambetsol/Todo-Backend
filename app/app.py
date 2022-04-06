b01a13811e077db6a1cf5b052180fe8c2a9809d4
Ayush Gautam <Ayush.Gautam@betsol.com>
2022-04-06 15:11:32 +0530

    push

--- app/app.py
+++ app/app.py
@@ -14,8 +14,11 @@ logger = custom_logger(logger)
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/SampleDb'
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://maepgdupzvcqru:2de24244b6ca685cd41dc98ae29c30c448ded6a6bcb0ff02afbd66dd5a35730f@ec2-52-73-155-171.compute-1.amazonaws.com:5432/dbmevkl5rhd3tg'
+    
+    @app.routes('/')
+    def home():
+        return "hello-world"
     from app.todoApp.model.todo_list_model import Todo
     db.init_app(app)
     migrate.init_app(app, db)
@@ -25,6 +28,3 @@ def create_app(test_config=None):
 
     return app
 
-@app.routes('/')
-def home():
-    return "hello-world"
