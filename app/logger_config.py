4198d164f023f95ce0eb23ff2e82233e059ec86c
Hitesh.Balegar <hitesh.balegar@betsol.com>
2022-04-07 00:02:33 +0530

    added std out logs

--- app/logger_config.py
+++ app/logger_config.py
@@ -1,10 +1,13 @@
 import logging
+import os
+import sys
 
 
 def custom_logger(logger):
     logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler = logging.FileHandler('app_service.log')
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+    handler = logging.StreamHandler(sys.stdout) if "DYNO" in os.environ else \
+        logging.FileHandler('app_service.log')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     return logger
